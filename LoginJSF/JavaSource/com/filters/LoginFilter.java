f1aef6815517e2307c6ccd8cdc74ae617178c97f
lenokp21 <lenokp21@hotmail.com>
2017-03-09 11:28:25 -0300

    1.0

---
+++ LoginJSF/JavaSource/com/filters/LoginFilter.java
@@ -0,0 +1,29 @@
+package com.filters;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+ 
+import org.jboss.security.SecurityAssociation;
+ 
+public class LoginFilter implements Filter {
+ 
+    @Override
+    public void destroy() {
+    }
+ 
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException,
+            ServletException {
+ 
+        String userName = SecurityAssociation.getPrincipal().getName();
+ 
+        System.out.println("Yeeey! Get me here and find me in the database: " + userName);
+ 
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+ 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+}
